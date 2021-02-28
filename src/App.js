d9f56eda7bb37456055546514ace2a2ef0b7e62d
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2021-02-27 20:14:26 -0500

    Finalized the news functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
 
-import newsCards from './components/NewsCards/newsCards';
+import NewsCards from './components/NewsCards/newsCards';
+import useStyles from './appStyles.js';
 
 const alanKey = '85237a5ee2b56daea3c94759cb754e7f2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    const classes = useStyles();
+
     // hooks in square brackets
+    // news articles
     const [newsArticles, setNewsArticles] = useState([]);
+    // current article, start at 0
+    const [activeArticle, setActiveArticle] = useState(-1); 
 
     // Arrow function with dependencies, if left empty, runs once
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command, articles }) => {
+            onCommand: ({ command, articles, number }) => {
                 if (command === 'newHeadlines') {
                     setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if (command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1)
+                } else if (command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if (parsedNumber > 20) {
+                        alanBtn().playText('Could not understand number. Please try again.');
+                    } else if (article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    }
                 }
             }
         })
@@ -23,8 +43,10 @@ const App = () => {
 
     return (
         <div>
-            <h1>Alan AI News Application</h1>
-            <newsCards articles={newsArticles} />
+            <div className={classes.logoContainer}>
+                <img src='https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/67659112_438501920211949_8507331483155824640_o.jpg?_nc_cat=105&ccb=3&_nc_sid=e3f864&_nc_ohc=0XbsedaNbEgAX-8MQr4&_nc_ht=scontent-ort2-1.xx&oh=6026373d402be2ab989917b98abe90aa&oe=60613C29' className={classes.alanLogo} alt="alan logo" />
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
         </div>
     );
 }
--- src/components/NewsCard/newsCard.js
+++ src/components/NewsCard/newsCard.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import classNames from 'classnames';
 
-const newsCard = ({ newsArticles }) => {
+import useStyles from './newsCardStyle.js';
+
+// Destructuring article
+const NewsCard = ({ article: {description, publishedAt, source, title, url, urlToImage }, i, activeArticle }) => {
+    const classes = useStyles();
+    const [elRefs, setElRefs] = useState([]);
+
+    // function to scroll to the card
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+    // create references to each card
+    useEffect(() => {
+        setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    // scroll to the card being read, gets called each time i /activeArticle / elRefs chance
+    useEffect(() => {
+        if (i === activeArticle && elRefs[activeArticle]) {
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs])
+
     return (
-        <div>
-            <h1>News Card</h1>
-        </div>
+        <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image={urlToImage || 'https://i2.wp.com/www.3x1.com/wp-content/uploads/2017/07/news.jpg?w=960'} />
+                <div className={classes.details}>
+                    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt).toDateString())}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+                </div>
+                <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography variant="h5" color="textSEcondary">{i + 1}</Typography>
+            </CardActions>
+        </Card>
     );
 };
 
-export default newsCard;
+export default NewsCard;
