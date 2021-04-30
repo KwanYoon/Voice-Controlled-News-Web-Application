1e58c1f1e6db9456109b806a546cb5363abe9cb3
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2021-04-30 14:54:17 -0400

    Updated Alan AI Logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import wordsToNumbers from 'words-to-numbers';
 
 import NewsCards from './components/NewsCards/newsCards';
 import useStyles from './appStyles.js';
+import AlanImage from './Icons/AlanAI.jfif';
 
 const alanKey = '85237a5ee2b56daea3c94759cb754e7f2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
@@ -49,7 +50,7 @@ const App = () => {
     return (
         <div className={classes.background}>
             <div className={classes.logoContainer}>
-                <input type='image' onClick={home} src='https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/67659112_438501920211949_8507331483155824640_o.jpg?_nc_cat=105&ccb=3&_nc_sid=e3f864&_nc_ohc=0XbsedaNbEgAX-8MQr4&_nc_ht=scontent-ort2-1.xx&oh=6026373d402be2ab989917b98abe90aa&oe=60613C29' className={classes.alanLogo} alt="alan logo" />
+                <input type='image' onClick={home} src={AlanImage} className={classes.alanLogo} alt="alan logo" />
             </div>
             <NewsCards articles={newsArticles} activeArticle={activeArticle} />
         </div>
